83a82b482ef86fb31c654ba842fec5d43f8faa28
yoelk <yoel.koenka@gmail.com>
2016-02-06 23:18:37 +0100

    pass the attributes another way

--- instrumentino/controllers/arduino.py
+++ instrumentino/controllers/arduino.py
@@ -34,22 +34,28 @@ class Arduino(Controller):
 class ArduinoChannelIn_AnalolgInPin(DataChannelIn):
     '''An analog pin input channel.
     '''
-    
+
+    data_bits = Arduino.BITS_NUM_ANALOG_IN_PIN
+    type_str = Arduino.CHANNEL_TYPE_STR_ANALOG_IN
+    max_input_value = Arduino.MAX_VALUE_ANALOG_IN_PIN
+    '''Set necessary attributes.
+    '''
+            
     def __init__(self, **kwargs):
-        kwargs['data_bits'] = Arduino.BITS_NUM_ANALOG_IN_PIN
-        kwargs['type_str'] = Arduino.CHANNEL_TYPE_STR_ANALOG_IN
-        kwargs['max_input_value'] = Arduino.MAX_VALUE_ANALOG_IN_PIN
         super(ArduinoChannelIn_AnalolgInPin, self).__init__(**kwargs)
     
     
 class ArduinoChannelIn_DigitalPin(DataChannelIn):
     '''A digital pin input channel.
     '''
+
+    data_bits = Arduino.BITS_NUM_DIGITAL_PIN
+    type_str = Arduino.CHANNEL_TYPE_STR_DIGITAL
+    max_input_value = Arduino.MAX_VALUE_DIGITAL_PIN    
+    '''Set necessary attributes.
+    '''
     
     def __init__(self, **kwargs):
-        kwargs['data_bits'] = Arduino.BITS_NUM_DIGITAL_PIN
-        kwargs['type_str'] = Arduino.CHANNEL_TYPE_STR_DIGITAL
-        kwargs['max_input_value'] = Arduino.MAX_VALUE_DIGITAL_PIN
         super(ArduinoChannelIn_DigitalPin, self).__init__(**kwargs)
         
     def do_first_when_online(self):
@@ -62,11 +68,14 @@ class ArduinoChannelIn_DigitalPin(DataChannelIn):
 class ArduinoChannelOut_DigitalPin(DataChannelOut):
     '''A digital pin output channel.
     '''
+
+    data_bits = Arduino.BITS_NUM_DIGITAL_PIN
+    type_str = Arduino.CHANNEL_TYPE_STR_DIGITAL
+    max_output_value = Arduino.MAX_VALUE_DIGITAL_PIN    
+    '''Set necessary attributes.
+    '''
     
     def __init__(self, **kwargs):
-        kwargs['data_bits'] = Arduino.BITS_NUM_DIGITAL_PIN
-        kwargs['type_str'] = Arduino.CHANNEL_TYPE_STR_DIGITAL
-        kwargs['max_output_value'] = Arduino.MAX_VALUE_DIGITAL_PIN
         super(ArduinoChannelOut_DigitalPin, self).__init__(**kwargs)
         
     def do_first_when_online(self):
@@ -79,12 +88,15 @@ class ArduinoChannelOut_DigitalPin(DataChannelOut):
 class ArduinoChannelInOut_DigitalPin(DataChannelInOut):
     '''A digital pin output channel.
     '''
+
+    data_bits = Arduino.BITS_NUM_DIGITAL_PIN
+    type_str = Arduino.CHANNEL_TYPE_STR_DIGITAL
+    max_input_value = Arduino.MAX_VALUE_DIGITAL_PIN
+    max_output_value = Arduino.MAX_VALUE_DIGITAL_PIN    
+    '''Set necessary attributes.
+    '''
     
     def __init__(self, **kwargs):
-        kwargs['data_bits'] = Arduino.BITS_NUM_DIGITAL_PIN
-        kwargs['type_str'] = Arduino.CHANNEL_TYPE_STR_DIGITAL
-        kwargs['max_input_value'] = Arduino.MAX_VALUE_DIGITAL_PIN
-        kwargs['max_output_value'] = Arduino.MAX_VALUE_DIGITAL_PIN
         super(ArduinoChannelInOut_DigitalPin, self).__init__(**kwargs)
         
     def do_first_when_online(self):
@@ -97,10 +109,12 @@ class ArduinoChannelInOut_DigitalPin(DataChannelInOut):
 class ArduinoChannelOut_PwmPin(DataChannelOut):
     '''A PWM pin output channel.
     '''
+
+    data_bits = Arduino.BITS_NUM_DIGITAL_PIN
+    type_str = Arduino.CHANNEL_TYPE_STR_PWM
+    max_output_value = Arduino.MAX_VALUE_PWM_PIN    
+    '''Set necessary attributes.
+    '''
     
     def __init__(self, **kwargs):
-        kwargs['data_bits'] = Arduino.BITS_NUM_DIGITAL_PIN
-        kwargs['type_str'] = Arduino.CHANNEL_TYPE_STR_PWM
-        kwargs['max_output_value'] = Arduino.MAX_VALUE_PWM_PIN
         super(ArduinoChannelOut_PwmPin, self).__init__(**kwargs)
-
