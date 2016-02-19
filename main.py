2bb63f3493bf81451543b82f5b7f03e2332fd41d
yoelk <yoel.koenka@gmail.com>
2016-02-19 23:46:12 +0100

    clean up a bit

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from instrumentino.variables import AnalogVariablePercentage,\
     DigitalVariableOnOff
 from instrumentino.controllers.arduino import Arduino, ArduinoChannelIn_AnalolgInPin,\
     ArduinoChannelInOut_DigitalPin, ArduinoChannelOut_DigitalPin
+from instrumentino.screens.automation import Action
+from instrumentino.controllers import Controller
+import inspect
 
 class AnalogInVariables(Component):
     '''An array of analog input variables
@@ -39,45 +42,35 @@ class DigitalInOutVariables(Component):
 
         super(DigitalInOutVariables, self).__init__(**kwargs)
 
-
 if __name__ == '__main__':
-
     app = InstrumentinoApp()
-    
+
     # Add controllers
     arduino = Arduino()
     app.add_controller(arduino)
-    
-    # ProfileLoader Testing
-#    app.remove_controller(name=controller.name)
-#    app.remove_controllers()
     
     # Add channels
  
     # Add components
     app.add_component(AnalogInVariables(ch_class=ArduinoChannelIn_AnalolgInPin, controller=arduino, channels_numbers=[0,1], sampling_rate=10))
     app.add_component(DigitalInOutVariables(ch_class=ArduinoChannelInOut_DigitalPin, controller=arduino, channels_numbers=[2,3], sampling_rate=10))
-    
-    
-    
-    # ProfileLoader Testing: 
-#    app.remove_component(name='MFC1')
-#    app.remove_components()
 
-#     app.add_component(MassFlowController(name='MFC1', 
-#                                          flow_ch_in=flow1_channel_in_out, flow_ch_out=flow1_channel_in_out,
-#                                          enable_ch_in=enable1_channel_in_out, enable_ch_out=enable1_channel_in_out,))
-#     app.add_component(MassFlowController(name='MFC2', 
-#                                          flow_ch_in=flow2_channel_in_out, flow_ch_out=flow2_channel_in_out,
-#                                          enable_ch_in=enable2_channel_in_out, enable_ch_out=enable2_channel_in_out,))
-#     app.add_component(MassFlowController(name='MFC3', 
-#                                          flow_ch_in=flow3_channel_in_out, flow_ch_out=flow3_channel_in_out,
-#                                          enable_ch_in=enable3_channel_in_out, enable_ch_out=enable3_channel_in_out,))
-
+    # Define actions
+    class Action1(Action):
+        '''An example action
+        '''
+         
+        var1 = AnalogVariablePercentage()
+        '''An example variable
+        '''
+         
+        def on_start(self):
+            '''Describe here what the action does
+            '''
+            print self.var1.value
+    
     # Add actions
-    app.add_action(name='action1', function=lambda:1)
-    # ProfileLoader Testing: 
-#    app.remove_action(name='action1')
-#    app.remove_actions()
+    app.add_action(Action1())
 
+    # Run application
     app.run()
