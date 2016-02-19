9adad7adc156d8fb05785953d75faba7ab287a9b
yoelk <yoel.koenka@gmail.com>
2016-02-19 23:42:20 +0100

    Give priority to kwargs for getting attributes

--- instrumentino/cfg.py
+++ instrumentino/cfg.py
@@ -15,14 +15,19 @@ def check_for_necessary_attributes(obj, attributes_list, kwargs={}):
     If they're empty, check if attr value was passed to them through kwargs.
     '''
     
-    for attr in attributes_list:
-        if (not hasattr(obj, attr) or
-            not getattr(obj, attr)):
-            if set([attr]) <= set(kwargs):
-                setattr(obj, attr, kwargs[attr])
-            else:
-                # A necessary attribute is missing or empty
-                raise MissingNecessaryAttributeError
+    # First use all of the given kwargs
+    for attr_name in attributes_list:
+        if attr_name in kwargs.keys():
+            setattr(obj, attr_name, kwargs[attr_name])
+            attributes_list.remove(attr_name)
+    
+    # Now check if all of the necessary attributes exist
+    for attr_name in attributes_list:
+        if (not hasattr(obj, attr_name) or
+            not getattr(obj, attr_name)):
+            # A necessary attribute is missing or empty
+            raise MissingNecessaryAttributeError(attr_name)
+
 
 class MissingNecessaryAttributeError(RuntimeError):
     '''Raised when a necessary attribute is missing in a subclass.
