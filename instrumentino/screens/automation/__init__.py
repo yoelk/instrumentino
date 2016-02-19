1e00eabc6e555decbfb0f55d3572653be72cca42
yoelk <yoel.koenka@gmail.com>
2016-02-19 23:46:53 +0100

    define the action class in a new way

--- instrumentino/screens/automation/__init__.py
+++ instrumentino/screens/automation/__init__.py
@@ -3,8 +3,12 @@ from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.properties import ListProperty
 from .. import MyView
 from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.app import App
+from instrumentino.cfg import *
+from instrumentino.variables import Variable
 
-class MyAutomationView(TabbedPanel, MyView):
+class MyAutomationView(BoxLayout, MyView):
     '''The Automation view allows the user to create and run lists of actions (called methods)
     and lists of methods (sequences)
     '''
@@ -15,24 +19,38 @@ class MyAutomationView(TabbedPanel, MyView):
     
     def __init__(self, **kwargs):
         super(MyAutomationView, self).__init__(**kwargs)
+        self.run_items.item_strings = self.actions
+        del self.run_items.adapter.data[:]
+        self.run_items.adapter.data.extend(self.actions)
+        self.run_items._trigger_reset_populate()
+
         
 class Action(Widget):
     '''An action to be performed in the system
     '''
     
-    name = StringProperty('an action')
+    name = StringProperty()
     '''The action's name on the screen
     '''
-    function = ObjectProperty(None)
-    '''The code to be executed for this function
+    
+    arguments = ListProperty()
+    '''The arguments needed for this action
+    '''
+
+    on_start = ObjectProperty()
+    '''The code to be executed for this action
     '''
     
+    on_stop = ObjectProperty()
+    '''The code to be executed when the action is stopped
+    '''
+    
+    
     def __init__(self, **kwargs):
-        super(Action, self).__init__()
-        if "name" in kwargs:
-            self.name = kwargs["name"]
-        elif "function" in kwargs:
-            self.function = kwargs["function"]
-        else:
-            raise TypeError( "Invalid arguments to Action" )
-
+        check_for_necessary_attributes(self, ['on_start'], kwargs)
+        self.name = self.name or App.get_running_app().create_default_name(self)
+        
+        # Automatically populate the arguments' list by collecting all of the "Variable" instances we have.
+        self.arguments = get_attributes_of_type(self, Variable, kwargs)
+        
+        super(Action, self).__init__(**kwargs)
