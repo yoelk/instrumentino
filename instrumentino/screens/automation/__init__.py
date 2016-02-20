d6d5e128615531b37e66d24c53309f095ea6d688
yoelk <yoel.koenka@gmail.com>
2016-02-20 01:18:30 +0100

    Use a custom class to keep the list data

--- instrumentino/screens/automation/__init__.py
+++ instrumentino/screens/automation/__init__.py
@@ -7,25 +7,50 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
 from instrumentino.cfg import *
 from instrumentino.variables import Variable
+from kivy.uix.listview import ListItemButton
+from kivy.event import EventDispatcher
 
 class MyAutomationView(BoxLayout, MyView):
     '''The Automation view allows the user to create and run lists of actions (called methods)
     and lists of methods (sequences)
     '''
     
-    actions = []
-    '''The actions that the system should perform
+    action_classes = ListProperty()
+    '''The possible action classes
     '''
     
     def __init__(self, **kwargs):
+        check_for_necessary_attributes(self, ['action_classes'], kwargs)
+        
         super(MyAutomationView, self).__init__(**kwargs)
-        self.run_items.item_strings = self.actions
+
+        self.run_items.item_strings = ['1','2','3']
         del self.run_items.adapter.data[:]
-        self.run_items.adapter.data.extend(self.actions)
+        self.run_items.adapter.data.extend(['1','2','3'])
         self.run_items._trigger_reset_populate()
 
+
+class AutomationItem(ListItemButton):
+    '''A widget to choose and enter information for a chosen automation item
+    '''
+    
+    action_classes = ListProperty()
+    '''The possible action classes
+    '''
+    
+    chosen_action = ObjectProperty()
+    '''The currently chosen action
+    '''
+    
+    def __init__(self, **kwargs):
+        super(AutomationItem, self).__init__(**kwargs)
         
-class Action(Widget):
+        check_for_necessary_attributes(self, ['action_classes'], kwargs)
+                
+        # By default, choose the first defined action class
+        self.chosen_action = self.chosen_action or self.action_classes[0]()
+
+class Action(EventDispatcher):
     '''An action to be performed in the system
     '''
     
